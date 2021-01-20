c5c205095e5a6d80e2a0d05f00ecc95f73953dec
adam1920-code <75556463+adam1920-code@users.noreply.github.com>
2021-01-20 09:18:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -75,9 +75,9 @@ const { othermenu } require('./database/menu/othermenu')*/
 /******LOAD OF VCARD INPUT******/
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Nazwa🖤\n' // full name
-            + 'ORG:Owner Bot;\n' // the organization of the contact
-            + 'TEL;type=CELL;type=VOICE;waid=12542123926:+1 (254) 212-3926\n' // WhatsApp ID + phone number
+            + 'FN:Adam Oey19🖤\n' // full name
+            + 'ORG:Yeongsill Bot;\n' // the organization of the contact
+            + 'TEL;type=CELL;type=VOICE;waid=6283153843600:+62 831 5384-3600\n' // WhatsApp ID + phone number
             + 'END:VCARD'
 /******END OF VCARD INPUT******/
 
