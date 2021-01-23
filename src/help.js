308776c6a95c1dd115cfd8a117e509525d676476
adam1920-code <75556463+adam1920-code@users.noreply.github.com>
2021-01-23 17:16:30 +0700

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,6 +1,6 @@
 const help = (prefix) => {
 	return `
-「 *BOT_NAME* 」
+「 *YEONGSILL BOT* 」
 
 DONASI PULSA: *085880029049*
 
@@ -11,7 +11,6 @@ DONASI PULSA: *085880029049*
   │
   ├─ 🔱 ${prefix}info
   ├─ 🔱 ${prefix}blocklist
-  ├─ 🔱 ${prefix}chatlist
   ├─ 🔱 ${prefix}ping
   └─ 🔱 ${prefix}bugreport
 ◪ *MAKER*
@@ -19,71 +18,7 @@ DONASI PULSA: *085880029049*
   ├─ 🔱 ${prefix}sticker
   ├─ 🔱 ${prefix}stickergif
   ├─ 🔱 ${prefix}toimg
-  ├─ 🔱 ${prefix}tomp3
-  ├─ 🔱 ${prefix}bpink
-  ├─ 🔱 ${prefix}naruto
-  ├─ 🔱 ${prefix}flower
-  ├─ 🔱 ${prefix}3dtext
-  ├─ 🔱 ${prefix}crossfire
-  ├─ 🔱 ${prefix}silk
-  ├─ 🔱 ${prefix}wood
-  ├─ 🔱 ${prefix}flaming
-  ├─ 🔱 ${prefix}rainbow
-  ├─ 🔱 ${prefix}smoke
-  ├─ 🔱 ${prefix}pubg
-  ├─ 🔱 ${prefix}sky
-  ├─ 🔱 ${prefix}cslogo
-  ├─ 🔱 ${prefix}pantai
-  ├─ 🔱 ${prefix}silverbutton
-  ├─ 🔱 ${prefix}goldbutton
-  ├─ 🔱 ${prefix}burnpaper
-  ├─ 🔱 ${prefix}coffee
-  ├─ 🔱 ${prefix}lovepaper
-  ├─ 🔱 ${prefix}woodblock
-  ├─ 🔱 ${prefix}qowheart
-  ├─ 🔱 ${prefix}mutgrass
-  ├─ 🔱 ${prefix}epep
-  ├─ 🔱 ${prefix}woodenboards
-  ├─ 🔱 ${prefix}wolfmetal
-  ├─ 🔱 ${prefix}metalictglow
-  ├─ 🔱 ${prefix}8bit
-  ├─ 🔱 ${prefix}ttp
-  ├─ 🔱 ${prefix}herrypotter
-  ├─ 🔱 ${prefix}pubglogo
-  └─ 🔱 ${prefix}quotemaker
-  └─ 🔱 ${prefix}mafia
-◪ *MEDIA*
-  │
-  ├─ 🔱 ${prefix}trendtwit
-  ├─ 🔱 ${prefix}randomkpop
-  └─ 🔱 ${prefix}ytsearch
-◪ *EDUCATION*
-  │
-  ├─ 🔱 ${prefix}wiki
-  ├─ 🔱 ${prefix}wikien
-  ├─ 🔱 ${prefix}nulis
-  ├─ 🔱 ${prefix}quotes
-  ├─ 🔱 ${prefix}quotes2
-  └─ 🔱 ${prefix}artinama
-  └─ 🔱 ${prefix}artimimpi
-◪ *KERANG AJAIB*
-  │
-  ├─ 🔱 ${prefix}apakah
-  ├─ 🔱 ${prefix}kapankah
-  ├─ 🔱 ${prefix}rate
-  └─ 🔱 ${prefix}bisakah
-◪ *DOWNLOADER*
-  │
-  ├─ 🔱 ${prefix}pinterest
-  ├─ 🔱 ${prefix}ytmp3
-  ├─ 🔱 ${prefix}ytmp4
-  ├─ 🔱 ${prefix}tiktok
-  └─ 🔱 ${prefix}joox
-◪ *MEME*
-  │
-  ├─ 🔱 ${prefix}meme
-  └─ 🔱 ${prefix}memeindo
-◪ *GROUP*
+  ◪ *GROUP*
   │
   ├─ 🔱 ${prefix}opengc
   ├─ 🔱 ${prefix}closegc
@@ -94,7 +29,6 @@ DONASI PULSA: *085880029049*
   ├─ 🔱 ${prefix}tagall3
   ├─ 🔱 ${prefix}tagall4
   ├─ 🔱 ${prefix}tagall5
-  ├─ 🔱 ${prefix}add
   ├─ 🔱 ${prefix}kick
   ├─ 🔱 ${prefix}listadmins
   ├─ 🔱 ${prefix}linkgroup
@@ -106,54 +40,6 @@ DONASI PULSA: *085880029049*
   ├─ 🔱 ${prefix}delete
   ├─ 🔱 ${prefix}simih
   └─ 🔱 ${prefix}ownergroup
-◪ *SOUND*
-  │
-  ├─ 🔱 ${prefix}play
-  └─ 🔱 ${prefix}tts
-◪ *MUSIC*
-  │
-  ├─ 🔱 ${prefix}lirik
-  └─ 🔱 ${prefix}chord
-◪ *ISLAM*
-  │
-  └─ 🔱 ${prefix}quran
-◪ *STALK*
-  │
-  ├─ 🔱 ${prefix}tiktokstalk
-  └─ 🔱 ${prefix}igstalk
-◪ *WIBU*
-  │
-  ├─ 🔱 ${prefix}neonime
-  ├─ 🔱 ${prefix}pokemon
-  ├─ 🔱 ${prefix}loli
-  ├─ 🔱 ${prefix}waifu
-  ├─ 🔱 ${prefix}randomanime
-  ├─ 🔱 ${prefix}husbu
-  ├─ 🔱 ${prefix}husbu2
-  ├─ 🔱 ${prefix}wait
-  └─ 🔱 ${prefix}nekonime
-◪ *FUN*
-  │
-  ├─ 🔱 ${prefix}alay
-  ├─ 🔱 ${prefix}gantengcek
-  ├─ 🔱 ${prefix}watak
-  ├─ 🔱 ${prefix}hobby
-  ├─ 🔱 ${prefix}game
-  ├─ 🔱 ${prefix}bucin
-  ├─ 🔱 ${prefix}trust
-  ├─ 🔱 ${prefix}dare
-  └─ 🔱 ${prefix}simi
-  └─ 🔱 ${prefix}ssweb
-◪ *INFORMATION*
-  │
-  ├─ 🔱 ${prefix}bahasa
-  ├─ 🔱 ${prefix}kodenegara
-  ├─ 🔱 ${prefix}kbbi
-  ├─ 🔱 ${prefix}fakta
-  ├─ 🔱 ${prefix}infocuaca
-  ├─ 🔱 ${prefix}infogempa
-  ├─ 🔱 ${prefix}jadwaltvnow
-  └─ 🔱 ${prefix}covid
 ◪ *OWNER*
   │
   ├─ 🔱 ${prefix}setprefix
@@ -162,16 +48,6 @@ DONASI PULSA: *085880029049*
   ├─ 🔱 ${prefix}bcgc
   ├─ 🔱 ${prefix}clone
   └─ 🔱 ${prefix}clearall
-◪ *OTHER*
-  │
-  ├─ 🔱 ${prefix}send
-  ├─ 🔱 ${prefix}wame
-  ├─ 🔱 ${prefix}exe
-  ├─ 🔱 ${prefix}qrcode
-  ├─ 🔱 ${prefix}afk
-  ├─ 🔱 ${prefix}timer
-  ├─ 🔱 ${prefix}fml
-  └─ 🔱 ${prefix}fml2
 ◪ *ADAM OEY19*
 `
 }
