2bbbca4faab48ed8e84bd069ba1a6f8b6ff5c958
adam1920-code <75556463+adam1920-code@users.noreply.github.com>
2021-02-09 14:00:19 +0700

    Update help.js

--- src/help.js
+++ src/help.js
@@ -7,13 +7,28 @@ BUAT BOT    : *HANYA 35K*
 FOLLOW IG   : *@adam_oey107*
 NOTE : *BERI DELAY 5 DETIK AGAR TIDAK TERJADI SPAM*
 
-◪ *INFO*
-  ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: ADAM OEY19
-◪ *ABOUT*
-  ├─ 🔱 ${prefix}play [judul lagu]
-  └─ 🔱 ${prefix}joox [judul lagu]
-◪ *ADAM OEY19*
+*INI ADALAH MENU SIMPLE YEONGSILL BOT
+✘ ▬▬▬▬ ஜ۩۞۩ஜ ▬▬▬▬ ✘
+        𝙇𝙞𝙨𝙩 𝘾𝙤𝙢𝙢𝙖𝙣𝙙
+✘ ▬▬▬▬ ஜ۩۞۩ஜ ▬▬▬▬ ✘
+● *!sticker*
+● *_!stickergif_*
+● *_!toimg_*
+● *_!tts_*
+● *_!joox [judul]_*
+● *_!play [judul]_*
+● *_!lirik [judul]_*
+● *_!ytmp3 [link yt]_*
+● *_!toimg_*
+● *_!neko_*
+● *_!pinterest_*
+● *_!ocr_*
+● *_!wiki_*
+● *_!spamcall [62831555555]_*
+● *_!ytmp3 [link]_*
+● *_!ytmp4 [link]_*
+вσт ιηι ∂ιвυαт σℓєн:
+A.D.A.M ✔
 `
 }
 
