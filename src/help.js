0c101757adf5cbc2c786098d0c7b6e2590bd65e0
adam1920-code <75556463+adam1920-code@users.noreply.github.com>
2021-02-16 09:09:58 +0700

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,18 +1,25 @@
 const help = (prefix) => {
 	return `
-「 *MUSIC BOT™* 」
+「 *YEONGSILL BOT™* 」
 
 DONASI PULSA: *085880029049*
 BUAT BOT    : *HANYA 35K*
 FOLLOW IG   : *@adam_oey107*
 NOTE : *BERI DELAY 5 DETIK AGAR TIDAK TERJADI SPAM*
 
-*INI ADALAH MENU MUSIC BOT™*
+*INI ADALAH MENU YEONGSILL BOT™*
 ✘ ▬▬▬▬ ஜ۩۞۩ஜ ▬▬▬▬ ✘
         𝙇𝙞𝙨𝙩 𝘾𝙤𝙢𝙢𝙖𝙣𝙙
 ✘ ▬▬▬▬ ஜ۩۞۩ஜ ▬▬▬▬ ✘
 ● *_!joox [judul]_*
 ● *_!play [judul]_*
+● *_!stiker*
+● *_!stickergif*
+● *_!toimg*
+● *_!wiki [querry]*
+● *_!tts id [teks]*
+● *_!spamcall [62831538444]_*
+
 вσт ιηι ∂ιвυαт σℓєн:
 A.D.A.M ✔
 `
